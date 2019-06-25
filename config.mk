277cc94c5be22e9631d4427cb9be4fcbe96f1e35
hanwckf <my375229675@gmail.com>
2019-06-26 00:55:27 +0800

    rename kernel-config

--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
 CROSS_COMPILE_TOOL = toolchains/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-KERNEL_CONFIG = rtd129x_raycloud_defconfig
+KERNEL_CONFIG = rtd129x_raycloud.config
 DTB = rtd-1296-raycloud-2GB.dtb
